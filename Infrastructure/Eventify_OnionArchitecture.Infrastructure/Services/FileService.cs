285c60fcdba1755cd6a7b1619c3c450274a5e8f5
QedirMahmudov <1qedirmahmudov@gmail.com>
2025-06-29 15:45:31 +0400

    infrastructure layer

---
+++ Infrastructure/Eventify_OnionArchitecture.Infrastructure/Services/FileService.cs
@@ -0,0 +1,21 @@
+﻿using Eventify_OnionArchitecture.Application.Abstractions.Services;
+
+namespace Eventify_OnionArchitecture.Infrastructure.Services
+{
+    public class FileService : IFileService
+    {
+        public async Task SaveTextAsync(string text, string path)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(path))
+                    throw new ArgumentNullException(nameof(path));
+                await File.WriteAllTextAsync(text, path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error:{ex}");
+            }
+        }
+    }
+}
---
+++ Infrastructure/Eventify_OnionArchitecture.Infrastructure/Services/TextService.cs
@@ -0,0 +1,26 @@
+﻿using Eventify_OnionArchitecture.Application.Abstractions.Services;
+using Eventify_OnionArchitecture.Application.DTOs;
+using System.Text;
+
+namespace Eventify_OnionArchitecture.Infrastructure.Services
+{
+    public class TextService : ITextService
+    {
+        public string FormatTextForEvent(IEnumerable<EventDTO> eventItems)
+        {
+            if (eventItems is null)
+                throw new ArgumentNullException(nameof(eventItems));
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (var item in eventItems)
+            {
+                if (eventItems is not null)
+                    stringBuilder.AppendLine($"Event: {item.Title} - Location: {item.Location} - Date:{item.EventDate.ToString("HH:mm - dd/MM/yyyy")}");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+
+        }
+    }
+}
